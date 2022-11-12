e7fbb54e45970849a301ddd266e76d3b12dad0a0
Adi-Marom <adimaro@bgu.ac.il>
2022-11-12 10:00:01 +0200

    fix the edgeDict change the type of the weight to int

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ revEdgeDict = {}
 # edgeDict["1"]=5
 # print(edgeDict["1"])
 i = 0
+maxWeight = 0
 with open("soc-sign-bitcoinotc.csv", 'r') as data:
     for line in csv.reader(data):
         if i == 0:
             i += 1
             continue
         if line[0] in edgeDict.keys():
-            edgeDict[line[0]][line[1]] = line[2]
+            edgeDict[line[0]][line[1]] = int(line[2])
+            if maxWeight < int(line[2]):
+                maxWeight = int(line[2])
         else:  # if value not exist,create a new dic as value {node:{node:weight}}
-            valueWightDic = {line[1]: line[2]}
+            valueWightDic = {line[1]: int(line[2])}
             edgeDict[line[0]] = valueWightDic
+            if maxWeight < int(line[2]):
+                maxWeight = int(line[2])
 
-print(edgeDict["6"])
+print(maxWeight)
+print(len(edgeDict.keys()))
 
 
 def load_graph(path):
