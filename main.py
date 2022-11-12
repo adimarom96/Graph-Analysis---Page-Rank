af39dc53e8d3e5b573af74af86eff43bd330ac5a
HadarSabati <hadarsabati@gmail.com>
2022-11-12 10:01:04 +0200

    stated calculate_page_rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-import pandas as pd
+#import pandas as pd
 
 # df = pd.read_csv('soc-sign-bitcoinotc.csv')
 # print(df.to_string())
@@ -26,10 +26,28 @@ print(edgeDict["6"])
 def load_graph(path):
     return '1'
 
+maxWight=10 #todo: delete afte merge
 
-def calculate_page_rank(beta, epcil, maxIterations):
-    return '1'
+def calculate_page_rank(beta=0.85, epcil=0.001, maxIterations=20):
+    n = len(edgeDict.keys())
 
+    #first iteration - each node gets r=1/n
+    prevPRiter = {node:calc_W_Rank(node, 1/n) for k in edgeDict.keys()}
+    currPRiter = {}
+
+    delta = 1 #todo:update with function
+
+    while maxIterations > 0 and delta > epcil: #stop if delta is smaller than epcil or we reached maxIterations
+        currPRiter =
+
+
+    print(currPRiter)
+
+def calc_W_Rank(node, rank):
+    weight = #todo
+    return rank*weight/maxWight
+
+calculate_page_rank()
 
 def get_PageRank(node_name):
     return '1'
